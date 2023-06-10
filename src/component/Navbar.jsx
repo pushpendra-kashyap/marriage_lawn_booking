6f05fd89acef786ebbdf016d65f10669e84d654b
Pushpendra <pushpendrakumarkashyap0@gmail.com>
2023-06-10 08:19:03 +0530

    change in navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
             to="/"
             className="text-Black text-2xl animate-bounce font-semibold font-serif"
           >
-            marriage lawn
+            Wedding Lawn
           </Link>
           <div>
             <div className="text-black items-center hidden md:flex md:items-center gap-10  ">
